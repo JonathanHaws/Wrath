6bea4d2afc38378b24eec70504228d1575fe03e4
JonathanHaws <jonathanmhaws1@gmail.com>
2025-05-04 13:23:50 -0600

    head tracking connected to bone working

--- project/scripts/head_follow.gd
+++ project/scripts/head_follow.gd
@@ -2,16 +2,12 @@ extends Node3D
 @export var target = Node
 @export var skeleton : Node
 @export var bone_name : String
-@export var influence_multiplier = .5
-@export var turn_speed = .23
+@export var offset = Vector3(0, 1, 0) 
+#@export var influence_multiplier = .5
+#@export var turn_speed = .23
 
 #made sure node 3d is not parented to skeleton in any way to avoid async updating of transform confusion
 var up_vector: Vector3
-
-func _ready() -> void:
-	var bone = skeleton.find_bone(bone_name)
-	var pose = skeleton.get_bone_global_pose(bone)
-	up_vector = (pose.origin - global_position).normalized()
 	
 func _process(delta: float) -> void:
 	if not target: return
@@ -20,9 +16,10 @@ func _process(delta: float) -> void:
 	var bone = skeleton.find_bone(bone_name)
 	var pose = skeleton.get_bone_global_pose(bone)
 	
+	global_transform.origin = skeleton.global_transform * (pose.origin + offset)
 	look_at(target.global_position, Vector3.UP, true)
-	var new_rotation = Quaternion.from_euler(Vector3(rotation))
 	
+	var new_rotation = Quaternion.from_euler(Vector3(rotation))
 	skeleton.set_bone_pose_rotation(bone, new_rotation)
 
 
