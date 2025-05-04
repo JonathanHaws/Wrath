23cdfdef6d99c5ac87521ed4a887e37574e42583
JonathanHaws <jonathanmhaws1@gmail.com>
2025-05-04 12:46:58 -0600

    head follow without bone override

--- project/scripts/head_follow.gd
+++ project/scripts/head_follow.gd
@@ -1,67 +1,30 @@
-extends BoneAttachment3D
+extends Node3D
 @export var target = Node
+@export var skeleton : Node
+@export var bone_name : String
 @export var influence_multiplier = .5
-@export var turn_speed = 1
-@export var range_horizontal = 45
-@export var range_vertical = 10
-var initial_rotation: Quaternion
-@export var use_model_front = false
-var initial_transform: Transform3D
+@export var turn_speed = .23
+
+#made sure node 3d is not parented to skeleton in any way to avoid async updating of transform confusion
+var up_vector: Vector3
 
 func _ready() -> void:
-	#initial_rotation = global_transform.basis.get_rotation_quaternion()
-	#initial_transform = transform
-	override_pose = false
-	pass
+	var bone = skeleton.find_bone(bone_name)
+	var pose = skeleton.get_bone_global_pose(bone)
+	up_vector = (pose.origin - global_position).normalized()
 	
 func _process(delta: float) -> void:
 	if not target: return
-	#offset it at the start of the frame
+	if not skeleton: return
 	
+	var bone = skeleton.find_bone(bone_name)
+	var pose = skeleton.get_bone_global_pose(bone)
 	
-	look_at(target.global_position,Vector3(0,1,0),use_model_front)
+	look_at(target.global_position, Vector3.UP, true)
+	var new_rotation = Quaternion.from_euler(Vector3(rotation))
 	
-	var skel := get_parent() as Skeleton3D
-	var idx := skel.find_bone(bone_name)
-	if idx < 0: return
-	
-	var bone = skel.get_bone_global_pose(idx) 
-	
-	var direction = (target.global_position - global_position).normalized()
-	bone.basis = Basis().looking_at(direction, Vector3.UP)
-	
-	skel.set_bone_global_pose(idx, bone)
-
-	
+	skeleton.set_bone_pose_rotation(bone, new_rotation)
 
 
-
-	#var euler: Vector3 = global_transform.basis.get_rotation_quaternion().get_euler() * rad_to_deg(1)
-	#print(euler)
-	#var clamped_y_rotation = clamp(euler.y, -range_horizontal, range_horizontal)
-	#
-	#var new_rot = Quaternion(Vector3(0, 1, 0), deg_to_rad(clamped_y_rotation))
-	#global_transform.basis = Basis(new_rot)
 	
-	
-	
-	
-	
-	#rotate_x(deg_to_rad(rotation_offset.x))
-	#rotate_y(deg_to_rad(rotation_offset.y))
-	#rotate_z(deg_to_rad(rotation_offset.z))
-	
-	#var skel := get_parent() as Skeleton3D
-	#var idx := skel.find_bone(bone_name)
-	#if idx < 0: return
-	#var pose := skel.get_bone_global_pose(idx)
-	#var to_target = target.global_position - get_parent().get_bone_global_pose(skel.find_bone(bone_name)).origin
-	##print(to_target)
-	#
-	#look_at(target.global_position)
-	#
-	#pose.basis = pose.basis.looking_at(to_target.normalized(), local_axis.normalized())
-	#skel.set_bone_global_pose_override(idx, pose, 1.0, true)
-
-				
 	
