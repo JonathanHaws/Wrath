fbff04fa894346469b5fedd2374d0564432a3d34
JonathanHaws <jonathanmhaws1@gmail.com>
2025-05-04 13:24:50 -0600

    accidental incomplete previous commit

--- project/scripts/head_follow.gd
+++ project/scripts/head_follow.gd
@@ -2,7 +2,7 @@ extends Node3D
 @export var target = Node
 @export var skeleton : Node
 @export var bone_name : String
-@export var offset = Vector3(0, 1, 0) 
+#@export var offset = Vector3(0, 1, 0) 
 #@export var influence_multiplier = .5
 #@export var turn_speed = .23
 
@@ -16,7 +16,8 @@ func _process(delta: float) -> void:
 	var bone = skeleton.find_bone(bone_name)
 	var pose = skeleton.get_bone_global_pose(bone)
 	
-	global_transform.origin = skeleton.global_transform * (pose.origin + offset)
+	global_transform.origin = skeleton.global_transform * pose.origin
+
 	look_at(target.global_position, Vector3.UP, true)
 	
 	var new_rotation = Quaternion.from_euler(Vector3(rotation))
