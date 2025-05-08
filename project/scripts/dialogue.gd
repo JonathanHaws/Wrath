e0883e4a39d6bf51c9d43c002ad293b7413d8587
JonathanHaws <jonathanmhaws1@gmail.com>
2025-05-08 13:06:39 -0600

    dialouge entire conversation animations like background and head looking influence / target script that uses groups and not editable children / other fixes and tweaks

--- project/scripts/dialogue.gd
+++ project/scripts/dialogue.gd
@@ -1,16 +1,19 @@
 extends AnimationPlayer
 @export var area: Area3D
 @export var player_group: String = "player"
+@export var range: AnimationPlayer
 @export var start_dialogue: StringName = "1"
 var in_range = false
 
 func _on_body_entered(body):
 	if not body.is_in_group(player_group): return
+	if range: range.queue("entered") 
 	in_range = true
 	play()
 
 func _on_body_exited(body):
 	if not body.is_in_group(player_group): return
+	if range: range.queue("exited") 
 	in_range = false
 	var progress = get_current_animation_position()
 	if (progress == 0 or progress == 1) : return
@@ -18,7 +21,12 @@ func _on_body_exited(body):
 
 func _on_animation_changed(new: String, old: String):
 	if not in_range: 
+		play(start_dialogue)
 		pause()
+
+func _on_animation_finished(anim_name: String):
+	play(start_dialogue)
+	pause()
 
 func freeze()->void:
 	if in_range:
@@ -29,6 +37,7 @@ func _ready():
 		area.body_entered.connect(_on_body_entered)
 		area.body_exited.connect(_on_body_exited)	
 	animation_changed.connect(_on_animation_changed)
+	animation_finished.connect(_on_animation_finished)
 	play(start_dialogue)
 	pause()
 
---
+++ project/scripts/target.gd
@@ -0,0 +1,21 @@
+extends Node3D
+@export var target_group: String
+var target: Node3D = null
+
+func get_closest_from_group_3d(group: String) -> Node3D:
+	var closest = null
+	var min_dist = INF
+	for node in get_tree().get_nodes_in_group(group):
+		if not node is Node3D:
+			continue
+		var dist = global_position.distance_squared_to(node.global_position)
+		if dist < min_dist:
+			min_dist = dist
+			closest = node
+	return closest
+	
+func _process(delta):
+	if target == null:
+		target = get_closest_from_group_3d(target_group)
+	if target != null:
+		global_position = target.global_position
