09a4e3e636a0a5d39e755d26ab8d0ed24b3002b3
JonathanHaws <jonathanmhaws1@gmail.com>
2025-05-12 12:27:11 -0600

    updated custom font to include numbers aswell as added damage spread for luck and variance for attacking areas

--- project/scripts/attacking_area.gd
+++ project/scripts/attacking_area.gd
@@ -1,15 +1,17 @@
 extends Node
 @export var damage = 1
+@export var damage_spread := 0
 @export var group = ""
 
 func _on_body_entered(body)-> void: 
 	if group != "" and body.is_in_group(group): return
-	if "health" in body: body.health -= damage
+	var final_damage = damage + randi_range(-damage_spread, damage_spread)
+	if "health" in body: body.health -= final_damage 
 	if "hurt" in body:
 		var arg_count = body.get_method_argument_count("hurt")
 		if arg_count >= 3:
-			body.call("hurt", damage, group, self.global_position)
+			body.call("hurt", final_damage, group, self.global_position)
 		elif arg_count == 2:
-			body.call("hurt", damage, group)
+			body.call("hurt", final_damage, group)
 		elif arg_count == 1:
-			body.call("hurt", damage)
+			body.call("hurt", final_damage)
