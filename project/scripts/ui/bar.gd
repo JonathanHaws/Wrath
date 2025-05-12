0e6fb6c49cc3fb421eae0597a7195d9f6d2bba4b
JonathanHaws <jonathanmhaws1@gmail.com>
2025-05-11 19:23:16 -0600

    status difference bars for the players health and stamina

--- project/scripts/ui/bar.gd
+++ project/scripts/ui/bar.gd
@@ -3,6 +3,7 @@ extends ProgressBar
 @export var host: Node
 @export var property_name: String = "health"
 @export var max_property_name: String = "health"
+@export var difference_speed: float = 0.5
 
 func _on_timer_timeout() -> void:
 	$Change.value = value
@@ -14,9 +15,10 @@ func _on_value_changed(_new_value: float) -> void:
 	$Timer.start()
 	
 func _ready()-> void:
+	$Timer.wait_time = difference_speed
 	value = host.get(property_name)
 	max_value = host.get(max_property_name)
 	_on_value_changed(value)
-	
+
 func _process(_delta: float) -> void:
 	if host: value = host.get(property_name)
