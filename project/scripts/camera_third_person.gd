369510b296d9153fa5196944a955cd228c648b61
JonathanHaws <jonathanmhaws1@gmail.com>
2025-05-21 13:33:36 -0600

    camera angle limitations to avoid confusing front flipping, also simplfied lock on and tracking to not need pivot node or quaternion just a spring arm camera and default euler yxz rotation format

--- project/scripts/camera_third_person.gd
+++ project/scripts/camera_third_person.gd
@@ -1,5 +1,7 @@
 extends Camera3D
-@export var PIVOT: Node3D
+@export_range(-90.0, 90.0, 1.0, "Degrees") var pitch_min_deg: float = -80.0
+@export_range(-90.0, 90.0, 1.0, "Degrees") var pitch_max_deg: float = 80.0
+@export var SpringArm: SpringArm3D
 @export var MOUSE_SENSITIVITY = 0.003
 @export var LOCK_ON_SPEED = 7
 @export var LOCK_ON_OFFSET: float = 4.0
@@ -16,9 +18,8 @@ func _input(event: InputEvent) -> void:
 
 func rotate_mesh_towards_camera_xz(delta: float, mesh: Node3D, input_vector: Vector2, turn_speed: float = 16) -> void:
 	if input_vector.length() == 0: return
-	var direction = Vector3(input_vector.x, 0, input_vector.y).normalized()
-	var target_rotation = atan2(-direction.x, -direction.z) + PIVOT.rotation.y
-	mesh.rotation.y = lerp_angle(mesh.rotation.y, target_rotation, turn_speed * delta)
+	var input_angle = atan2(-input_vector.x, -input_vector.y)
+	mesh.global_rotation.y = lerp_angle(mesh.global_rotation.y, SpringArm.global_rotation.y + input_angle, turn_speed * delta)
 
 func _on_lock_on_area_entered(area: Node) -> void:
 	if area.name == "LockOn":
@@ -26,30 +27,38 @@ func _on_lock_on_area_entered(area: Node) -> void:
 
 func _lock_on(_delta: float)-> void:
 	
+	if not lock_on_activated: return
+	if not is_instance_valid(lock_on_target): return
+	
+	var current_rotation = SpringArm.global_rotation
+	SpringArm.look_at(lock_on_target.global_position - SpringArm.position, Vector3.UP)
+	var new_rotation = SpringArm.global_rotation
+	
+	SpringArm.global_rotation.x = clamp(
+		lerp_angle(current_rotation.x, new_rotation.x, LOCK_ON_SPEED * _delta),
+		deg_to_rad(pitch_min_deg),
+		deg_to_rad(pitch_max_deg)
+		)
+	SpringArm.global_rotation.y = lerp_angle(current_rotation.y, new_rotation.y, LOCK_ON_SPEED * _delta)
+	mouse_delta = Vector2.ZERO
+
+func _update_lock_on_indicator() -> void:
+	
 	if Input.is_action_just_pressed("lock_on"):
 		lock_on_activated = !lock_on_activated
 	
-	if not (lock_on_activated and lock_on_target and is_instance_valid(lock_on_target)):
+	if lock_on_activated and is_instance_valid(lock_on_target):
+		LOCK_ON_INDICATOR.visible = true
+		LOCK_ON_INDICATOR.position = unproject_position(lock_on_target.global_transform.origin)
+	else:
 		lock_on_activated = false
 		LOCK_ON_INDICATOR.visible = false
-		return
-
-	LOCK_ON_INDICATOR.visible = true;
-	LOCK_ON_INDICATOR.position = unproject_position(lock_on_target.global_transform.origin)
-	
-	var target_position = lock_on_target.global_transform.origin - Vector3(0, LOCK_ON_OFFSET, 0)
-	var current_rotation = PIVOT.global_transform.basis.get_rotation_quaternion()
-	PIVOT.look_at(target_position + PIVOT.position, Vector3.UP)
-	var new_rotation = PIVOT.global_transform.basis.get_rotation_quaternion()
-	PIVOT.global_transform.basis = Basis(current_rotation.slerp(new_rotation, LOCK_ON_SPEED * _delta))
-	mouse_delta = Vector2.ZERO
-
 
 func _ready() -> void:
 	LOCK_ON_AREA.area_entered.connect(_on_lock_on_area_entered)
 	
-#func _process(_delta: float) -> void:
-	#pass #update the indicator here to not have lag 
+func _process(_delta: float) -> void:
+	_update_lock_on_indicator()
 			
 func _physics_process(delta: float) -> void:
 	
@@ -65,9 +74,10 @@ func _physics_process(delta: float) -> void:
 	_lock_on(delta)
 	
 	if mouse_delta.length() > 0:
-		
-		var y_rot = Quaternion(Vector3.UP, -mouse_delta.x * MOUSE_SENSITIVITY)
-		var x_rot = Quaternion(PIVOT.basis.x.normalized(), -mouse_delta.y * MOUSE_SENSITIVITY)
-		PIVOT.basis = Basis(y_rot) * Basis(x_rot) * PIVOT.transform.basis
-
+		SpringArm.global_rotation.y -= mouse_delta.x * MOUSE_SENSITIVITY
+		SpringArm.global_rotation.x = clamp(
+			SpringArm.global_rotation.x - mouse_delta.y * MOUSE_SENSITIVITY,
+			deg_to_rad(pitch_min_deg),
+			deg_to_rad(pitch_max_deg)
+		)
 		mouse_delta = Vector2.ZERO
