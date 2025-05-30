60d9de45c23d4f0473d0119b4355adaf29c273fa
JonathanHaws <jonathanmhaws1@gmail.com>
2025-05-30 00:44:54 -0600

    floor texture and uv unwrapping, and checkpoint dependency pruning

--- project/scripts/checkpoint.gd
+++ project/scripts/checkpoint.gd
@@ -1,11 +1,11 @@
 extends Area3D
-@export var REAPER: CharacterBody3D
+@export var GROUP = "player"
 @export var CHECKPOINT_NODE: Node3D
 @export var CHECKPOINT_SCENE_PATH: String
 @export var ANIM: AnimationPlayer
 
 func _on_body_entered(body: Node) -> void:
-	if body != REAPER: return
+	if GROUP != "" and not body.is_in_group(GROUP): return
 
 	if Save.data.has("checkpoint_node_path") and get_path() == NodePath(Save.data["checkpoint_node_path"]) and \
 	   Save.data.has("checkpoint_scene_path") and Save.data["checkpoint_scene_path"] == get_tree().current_scene.scene_file_path:
