55a9a4904322a225de597fbaa95aa27bd6fd6b40
JonathanHaws <jonathanmhaws1@gmail.com>
2025-06-24 13:42:10 -0600

    introduction level rough draft and menu fixes with absolute vs relative path

--- scripts/ui/redirect.gd
+++ scripts/ui/redirect.gd
@@ -1,16 +1,18 @@
 extends Button
-
 @export var scene_to_spawn_path: String
 @export var node_to_replace: Node
 
+func _on_pressed() -> void:
+	if !node_to_replace: return
+	
+	var parent = node_to_replace.get_parent()
+	
+	node_to_replace.queue_free()
+	var scene = load(scene_to_spawn_path)
+	
+	if scene:
+		var instance = scene.instantiate()
+		parent.add_child(instance)
+
 func _ready() -> void:
 	pressed.connect(_on_pressed)
-
-func _on_pressed() -> void:
-	if node_to_replace:
-		var parent = node_to_replace.get_parent()
-		node_to_replace.queue_free()
-		var scene = load(scene_to_spawn_path)
-		if scene:
-			var instance = scene.instantiate()
-			parent.add_child(instance)
