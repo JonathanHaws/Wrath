99087c28caf67aacaf63239df6d774aa58e12f18
JonathanHaws <jonathanmhaws1@gmail.com>
2025-07-12 23:59:20 -0600

    pause improvements

---
+++ scripts/ui/pause_toggle.gd
@@ -0,0 +1,13 @@
+# Make sure process mode for this node is 'always' so when game pauses it doesnt pause aswell
+extends CanvasLayer
+func _ready() -> void: toggle(false)
+
+func toggle(paused: bool) -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE if paused else Input.MOUSE_MODE_CAPTURED
+	self.visible = paused
+	Engine.time_scale = 0 if paused else 1
+	get_tree().paused = paused
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("ui_cancel"):
+		toggle(not self.visible) 
