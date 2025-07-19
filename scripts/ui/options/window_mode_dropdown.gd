8cbbf1fca402f33f16e365c0cf412f3b6971c269
JonathanHaws <jonathanmhaws1@gmail.com>
2025-07-19 01:29:22 -0600

    window borderless and small folder reogrinization

--- scripts/ui/options/window_mode_dropdown.gd
+++ scripts/ui/options/window_mode_dropdown.gd
@@ -6,5 +6,11 @@ func _ready() -> void:
 
 func _on_window_mode_changed(index: int) -> void:
 	match index:
-		0: DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-		1: DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		0:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		1:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+		2:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
