053ad06a881d1c789cf89419881dbd822dda4454
JonathanHaws <jonathanmhaws1@gmail.com>
2025-08-19 22:45:13 -0600

    animation trigger

--- actors/characters/npcs/piglet/interaction.gd
+++ actors/characters/npcs/piglet/interaction.gd
@@ -1,12 +1,14 @@
 extends Area3D	
 @export var animation_player: AnimationPlayer	
-@export var animation_name: StringName = &""	# animation to play
+@export var animation_name: StringName = &""	## Animation to play
 @export var player_group_name: String = "player"
 @export var player_anim_group: String = "player_anim" ## Player AnimationPlayer To trigger in the player
 @export var player_anim: StringName = &"" ## Corresponding animation 
 
 @export var transform_node: Node3D	## Node3D that will be moved to match player (rotated, translated, transformed)
 @export var target_transform_node_group: String = "player_mesh"	## Node3D that will be moved to match player (rotated, translated, transformed)
+var original_global_transforms: Dictionary = {}  
+
 
 func _on_body_entered(body: Node3D) -> void:
 	if not body.is_in_group(player_group_name): return
@@ -15,11 +17,25 @@ func _on_body_entered(body: Node3D) -> void:
 	if animation_player.is_playing(): return
 	animation_player.play(animation_name)	
 
-func _match_transforms(duration: float = 0.0, target_weight: float = 1.0) -> void:
+func _match_transforms(duration: float = 0.0, transform_weight: float = 1.0, position_weight: Vector3 = Vector3.ONE) -> void:
 	if not transform_node: return
-	for target in get_tree().get_nodes_in_group(target_transform_node_group):
+	for target in get_tree().get_nodes_in_group(target_transform_node_group):	
 		
-		var middle_transform = transform_node.global_transform.interpolate_with(target.global_transform, target_weight)
+		if not original_global_transforms.has(transform_node):
+			original_global_transforms[transform_node] = transform_node.global_transform
+		if not original_global_transforms.has(target):
+			original_global_transforms[target] = target.global_transform
+
+		# interpolate per axis
+		var start = transform_node.global_transform
+		var end = target.global_transform
+		var middle_transform = start
+		middle_transform.origin = start.origin.lerp(end.origin, transform_weight)
+		middle_transform.origin = Vector3(
+			lerp(start.origin.x, end.origin.x, position_weight.x),
+			lerp(start.origin.y, end.origin.y, position_weight.y),
+			lerp(start.origin.z, end.origin.z, position_weight.z)
+		)
 		
 		var tween1 = create_tween()
 		tween1.tween_property(transform_node, "global_transform", middle_transform, duration)
