6d336396eb354201a6d85f9cc73377ad039e5ca7
JonathanHaws <jonathanmhaws1@gmail.com>
2025-08-21 17:44:53 -0600

    gluttony shooting vomit projectiles

--- actors/characters/npcs/piglet/interaction.gd
+++ actors/characters/npcs/piglet/interaction.gd
@@ -9,14 +9,15 @@ extends Node
 @export var target_anim_group: String = "player_anim" ## Animationg player group to trigger animation
 @export var target_anim: StringName = &"" ## Corresponding animation 
 
-@export_group("Interpolate Transforms") ## Remember animation tracks properties are update top to bottom in track list
+@export_group("Interpolate Transforms") 
+@export_subgroup("Transforms")
 @export var parent_transform: Node3D## Transform to be aligned
 @export var child_transform: Node3D	 ## Transform thats reset so only parent transform in this object needs to be aligned
 @export var target_parent_transform_group: String = "player_body" ## Transform in other other object to be aligned
 @export var target_child_transform_group: String = "player_mesh" ## Resets local transform so only parent transform has to be Aligned
 @export_subgroup("Interpolation")## Remember animation tracks properties are update top to bottom in track list
 @export_range(0.0, 10.0, 0.01, "or_greater") var duration_seconds: float = 0.0 ## Determines how long the tween takes to align transforms 
-@export_range(0.0, 1.0, 0.01) var weight: float = 0.5 ## Determines how much influences each transform has on the meeting middle point
+@export_range(0.0, 1.0, 0.01) var weight: float = 0.5 ## Determines who stays still versus who moves. Usually only want target to move (0). Middle is (0.5)
 
 func _on_body_entered(body: Node3D) -> void:
 	if not body.is_in_group(target_body_group): return
@@ -27,30 +28,21 @@ func _on_body_entered(body: Node3D) -> void:
 
 func _match_transforms() -> void:
 	
-	await get_tree().process_frame
-	var tween = null
-	if duration_seconds > 0.0:
-		tween = create_tween()
-		tween.set_parallel(true)
-	
 	for target_child_transform in get_tree().get_nodes_in_group(target_child_transform_group):
 		for target_parent_transform in get_tree().get_nodes_in_group(target_parent_transform_group):	
 			
-			# Find global middle point between the childrens local transforms
+			# Find global middle transform point between the childrens local transforms
 			var middle = child_transform.global_transform.interpolate_with(target_child_transform.global_transform, weight)
 			
-			if tween: 
-				tween.tween_property(child_transform, "global_transform", middle, duration_seconds)
-				tween.tween_property(parent_transform, "global_transform", middle, duration_seconds)
-				tween.tween_property(target_child_transform, "global_transform", middle, duration_seconds)
-				tween.tween_property(target_parent_transform, "global_transform", middle, duration_seconds)
+			if duration_seconds > 0.0:
+				var tween = create_tween()
+				tween.set_parallel(true)
+				for node in [parent_transform, target_parent_transform, child_transform, target_child_transform]:
+					tween.tween_property(node, "global_transform", middle, duration_seconds)
 			else: 
-				parent_transform.global_transform = middle
-				child_transform.global_transform = middle
-				target_parent_transform.global_transform = middle
-				target_child_transform.global_transform = middle
+				for node in [parent_transform, target_parent_transform, child_transform, target_child_transform]:
+					node.global_transform = middle
 
-				
 		
 func _trigger_corresponding_animation() -> void:
 	_match_transforms()	
