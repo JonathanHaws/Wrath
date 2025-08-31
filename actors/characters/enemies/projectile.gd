22c3f3b5caf441b1dbaf92d23553fed074bf2d03
JonathanHaws <jonathanmhaws1@gmail.com>
2025-08-31 16:55:20 -0600

    puddles

--- actors/characters/enemies/projectile.gd
+++ actors/characters/enemies/projectile.gd
@@ -1,10 +1,14 @@
 extends Node3D
 @export var speed = 15.0
 @export var destroy_area: Area3D
-@export var body_exclude_groups: Array[String] = []
+@export var body_exclude_groups: Array[String] = [] 
 @export var area_exclude_groups: Array[String] = []
 @export var destroy_on_every_body_entered: bool = true
 @export var destroy_on_every_area_entered: bool = false
+
+@export var collision_animation_player: AnimationPlayer
+@export var collision_animation_name: String = ""
+@export var collision_point_node: Node3D ## When an area or body or area is entered this moves to collision point 
 
 @export var gravity: float = 0.0  
 var velocity: Vector3
@@ -16,6 +20,32 @@ var velocity: Vector3
 @export var homing_group: String = "player_body"
 @export var homing_speed: float = 2.0  
 @export var homing_offset: Vector3 = Vector3(0, .5, 0)
+
+
+
+func _play_collision_animation():
+	if collision_animation_player and collision_animation_name != "":
+		collision_animation_player.play(collision_animation_name)
+		await collision_animation_player.animation_finished	
+	queue_free()
+
+func _on_body_entered(body: Node) -> void:
+	if destroy_on_every_body_entered:
+		for group in body_exclude_groups:
+			if body.is_in_group(group): return
+	else: 
+		for group in body_exclude_groups:
+			if not body.is_in_group(group): return
+	_play_collision_animation()
+
+func _on_area_entered(area: Area3D) -> void:
+	if destroy_on_every_area_entered:
+		for group in area_exclude_groups:
+			if area.is_in_group(group): return
+	else:
+		for group in area_exclude_groups:
+			if not area.is_in_group(group): return
+	_play_collision_animation()
 
 func _ready():
 	await get_tree().process_frame
@@ -44,6 +74,22 @@ func _ready():
 			else: # straight
 				velocity = to_target / time	
 
+func get_collision_point(move_vector: Vector3) -> Vector3:
+	var next_pos = global_position + move_vector
+	var space = get_world_3d().direct_space_state
+
+	var params = PhysicsRayQueryParameters3D.create(global_position, next_pos)
+	params.collide_with_bodies = true
+	params.collide_with_areas = true
+
+	var result = space.intersect_ray(params)
+	if result:
+		if collision_point_node:
+			collision_point_node.global_position = result.position
+		_play_collision_animation()
+		return result.position
+	return next_pos
+	
 func _process(delta):
 
 	if homing:	
@@ -55,24 +101,5 @@ func _process(delta):
 
 	if velocity.length() > 0.0:
 		look_at(global_position + velocity, Vector3.UP)
-		
-	global_position += velocity * delta
-
-func _on_body_entered(body: Node) -> void:
 	
-	if destroy_on_every_body_entered:
-		for group in body_exclude_groups:
-			if body.is_in_group(group): return
-	else: 
-		for group in body_exclude_groups:
-			if not body.is_in_group(group): return
-	queue_free()
-
-func _on_area_entered(area: Area3D) -> void:
-	if destroy_on_every_area_entered:
-		for group in area_exclude_groups:
-			if area.is_in_group(group): return
-	else:
-		for group in area_exclude_groups:
-			if not area.is_in_group(group): return
-	queue_free()
+	global_position += velocity * delta
