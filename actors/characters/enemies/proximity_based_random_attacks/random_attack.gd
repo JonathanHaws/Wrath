40d10dff7fcb314c60d4a51c7860aeb92c9dd5d4
JonathanHaws <jonathanmhaws1@gmail.com>
2025-09-10 00:39:22 -0600

    attacking script refactor to be node based component. Basically before had random attacks based of proximity as a script in the animation player. This was confusing as had to manage multiple parallel arrays for attack liklehood, radius, etc. Instead made this system which has each attack as a child node to animation player. This means related attack data is closer to eachother and also will make more changes and features to when attacks happen easier. Like a cooldowns or combos as the old attack system was getting pretty complex to update

---
+++ actors/characters/enemies/proximity_based_random_attacks/random_attack.gd
@@ -0,0 +1,34 @@
+extends Node
+# Child node to animation player to specify attacks
+
+@export var ATTACK_ANIMATION: String = "SLAM"
+@export var LIKELIHOOD_MULTIPLIER: float = 1.0 ## Easy value for adjusting the overall frequency
+@export var ATTACK_RADIUS: float = 50.0
+@export var ATTACK_LIKELEHOOD_PER_SECOND: Curve
+@export var AREA_MULTIPLIER: Area3D ## Eg. Only let backstab happen if behind... Area that specifies whats considered behind... 
+@export var TRIGGER_ANIMATIONS: Array[String] = ["CHASE"] ## Requires animation player to be playing this animation trigger an attack...
+
+@export_group("References")
+@export var TARGET: Node3D
+@export var BODY: Node3D
+
+func _physics_process(delta: float) -> void:
+
+	var player: AnimationPlayer = get_parent() as AnimationPlayer
+	if not player: return
+	if TRIGGER_ANIMATIONS.size() > 0 and not (player.current_animation in TRIGGER_ANIMATIONS): return
+
+	var proximity_likelihood = 0
+	if not BODY or not TARGET or not ATTACK_LIKELEHOOD_PER_SECOND: proximity_likelihood = 0
+	var distance = BODY.global_position.distance_to(TARGET.global_position)
+	if distance > ATTACK_RADIUS: proximity_likelihood = 0
+	proximity_likelihood = ATTACK_LIKELEHOOD_PER_SECOND.sample(clamp(distance / ATTACK_RADIUS, 0.0, 1.0))
+	
+	var area_likelihood: float = 1.0
+	if AREA_MULTIPLIER and not AREA_MULTIPLIER.overlaps_body(TARGET.target): area_likelihood = 0.0
+	
+	var chance = proximity_likelihood * area_likelihood * LIKELIHOOD_MULTIPLIER * delta
+	if randf() < chance:
+		player.play(ATTACK_ANIMATION)
+		
+	#print(chance)
