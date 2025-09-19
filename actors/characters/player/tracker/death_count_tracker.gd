4fcfacd4f55b587abe8d217d6dea238d765088ec
JonathanHaws <jonathanmhaws1@gmail.com>
2025-09-18 21:16:55 -0600

    bug fixes

--- actors/characters/player/tracker/death_count_tracker.gd
+++ actors/characters/player/tracker/death_count_tracker.gd
@@ -1,11 +1,15 @@
 extends Node
 @export var key: String
+@export var HITSHAPE: Node
 
 func _ready(): 
 	if not Save.data.has(key): Save.data[key] = 0
 
 func increase(): 
 	Save.data[key] += 1
+	
+	if HITSHAPE and "MAX_HEALTH" in HITSHAPE and "HEALTH" in HITSHAPE:
+		HITSHAPE.HEALTH = HITSHAPE.MAX_HEALTH
 	
 	if Save.data.has("respawn_data"):Save.data["respawn_data"].clear()
 	
