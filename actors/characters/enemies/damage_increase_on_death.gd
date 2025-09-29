9e48b68881f11e2627f1a150696a695ea92d1be3
JonathanHaws <jonathanmhaws1@gmail.com>
2025-09-28 18:50:02 -0600

    made it so you have to kill 35 enemies to double damage. For the future use wolfram alpha 1.05 ^ n = 2 to find out enemy doubling amount. Also made it so continuing save increases rests bug

--- actors/characters/enemies/damage_increase_on_death.gd
+++ actors/characters/enemies/damage_increase_on_death.gd
@@ -1,5 +1,7 @@
 extends Node
-@export var DAMAGE_INCREASE := 1.1
+@export var DAMAGE_INCREASE := 1.02 
+# 35 enemies to double damagewolfram alpha claculation to help 1.02 ^ n = 2
+
 @export var DAMAGE_KEY := ""
 
 func increase_player_damage() -> void:
--- menus/save/continue.gd
+++ menus/save/continue.gd
@@ -3,6 +3,10 @@ extends Control
 func _on_continue_pressed() -> void:
 	var save_files = Save.get_save_files()
 	if save_files.size() > 0:
+		
+		Save.load_save_data(save_files[0]["file_name"]) 
+		Save.data["rests"] = (Save.data.get("rests", 0) + 1)
+		Save.save_game()
 		Save.load_game(save_files[0]["file_name"])
 
 func _on_visibility_changed() -> void:
