81bd3a8c832ec4f20f60adbfa6cea1b0a262e6c1
JonathanHaws <jonathanmhaws1@gmail.com>
2025-10-03 17:05:37 -0600

    attack cooldowns dont start cooldowning until the attack is finished, overall script improvements, and multiple phases. By specifying an upper and lower bound of health when specific attacks can be triggered

--- actors/characters/enemies/random_attack.gd
+++ actors/characters/enemies/random_attack.gd
@@ -1,31 +1,42 @@
-extends Node
-# Child node to animation player to specify attacks based of promximity and other things
-
+extends Node # Child node to animation player to specify attacks based of randomness, promximity, and boss health
 @export var ATTACK_ANIMATION: String = "SLAM"
 @export var LIKELIHOOD_MULTIPLIER: float = 1.0 ## Easy value for adjusting the overall frequency
 @export var ATTACK_RADIUS: float = 10.0
 @export var ATTACK_LIKELEHOOD_PER_SECOND: Curve
 @export var AREA_MULTIPLIER: Area3D ## Eg. Only let backstab happen if behind... Area that specifies whats considered behind... 
-@export var TRIGGER_ANIMATIONS: Array[String] = ["CHASE"] ## Requires animation player to be playing this animation trigger an attack...
+@export var TRIGGER_ANIMATIONS: Array[String] = ["CHASE"] ## Requires animation player to be playing specific animations to trigger an attack... If no required trigger will trigger no matter the current animation
 @export var DISABLED: bool = false
 @export var COOLDOWN: float = 1.0
 var cooldown_remaining: float = 0.0
 
+@export_group("Phase") ## Used for disalbing certain attacks until the proper phase. Accelerating difficulty makes bosses exiciting 
+@export var PHASE_HITSHAPE: Node ## Percentage determined by health / max_health
+@export var HEALTH_HIGH_THRESHOLD: float = 10.0   ## 0.5 = 50%
+@export var HEALTH_LOW_THRESHOLD: float = 0.0   ## 0.5 = 50%
+
 @export_group("References")
-@export var TARGET: Node3D
+@export var ANIM: AnimationPlayer ## Animation player to trigger
+@export var TARGET: Node3D ## Used to calculate the proximity or distance to weigth attacks 
 @export var BODY: Node3D
+
+func _ready() -> void:
+	if not ANIM: ANIM = get_parent() as AnimationPlayer
 
 func _physics_process(delta: float) -> void:
 
+	if not ANIM: return
 	if DISABLED: return
 
 	if cooldown_remaining > 0.0:
+		if ANIM.current_animation == ATTACK_ANIMATION: return
 		cooldown_remaining -= delta
 		return
 
-	var player: AnimationPlayer = get_parent() as AnimationPlayer
-	if not player: return
-	if TRIGGER_ANIMATIONS.size() > 0 and not (player.current_animation in TRIGGER_ANIMATIONS): return
+	if PHASE_HITSHAPE and "HEALTH" in PHASE_HITSHAPE and "MAX_HEALTH" in PHASE_HITSHAPE:
+		var hp_ratio: float = float(PHASE_HITSHAPE.HEALTH) / float(PHASE_HITSHAPE.MAX_HEALTH)
+		if hp_ratio > HEALTH_HIGH_THRESHOLD or hp_ratio < HEALTH_LOW_THRESHOLD: return
+
+	if TRIGGER_ANIMATIONS.size() > 0 and not (ANIM.current_animation in TRIGGER_ANIMATIONS): return
 
 	var proximity_likelihood = 0
 	if not BODY or not TARGET or not ATTACK_LIKELEHOOD_PER_SECOND: proximity_likelihood = 0
@@ -38,7 +49,7 @@ func _physics_process(delta: float) -> void:
 	
 	var chance = proximity_likelihood * area_likelihood * LIKELIHOOD_MULTIPLIER * delta
 	if randf() < chance:
-		player.play(ATTACK_ANIMATION)
+		ANIM.play(ATTACK_ANIMATION)
 		cooldown_remaining = COOLDOWN
 		
 	#print(chance)
