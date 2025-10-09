5f085fcbbe4b63d011ed5ca715bf61dc4540c8c3
JonathanHaws <jonathanmhaws1@gmail.com>
2025-10-09 01:46:31 -0600

    fixed bug at ending where player could still move by deleting them in escape cinematic. Also tried to add going to credits screen after game is done but the credits screen and title screen are coupled. Ideally this needs to be fixed so that going to any menu from any other state of the game is easy as it should be modular

--- menus/redirect.gd
+++ menus/redirect.gd
@@ -1,4 +1,4 @@
-extends Button
+extends Node # Can be used on anything but most likely button
 @export_file("*.tscn") var scene_file: String
 ## if no node to replace is specified will replace the entire tree
 @export var node_to_replace: Node
@@ -8,7 +8,7 @@ var scene = null
 func _ready() -> void:
 	if scene_file != "" and ResourceLoader.exists(scene_file):
 		scene = load(scene_file)
-	pressed.connect(_on_pressed)
+	if has_signal("pressed"): self.pressed.connect(_on_pressed)
 
 func _on_pressed() -> void:
 	if not scene: return
