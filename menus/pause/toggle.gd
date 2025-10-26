b47a943a2252b9f72d580b4754d2347b834686c1
JonathanHaws <jonathanmhaws1@gmail.com>
2025-10-26 14:48:25 -0600

    fixed bug of main menu still being paused after returning from gameplay

--- menus/pause/toggle.gd
+++ menus/pause/toggle.gd
@@ -1,6 +1,13 @@
-# Make sure process mode for this node is 'always' so when game pauses it doesnt pause aswell
+
+# Make sure in main mneu put a node2d with this script which is hidden...
+# That way if the player is coming back to the main menu from a pause menu the game will be unpaused 
+# If this is not done menu will seem unresponsive and glitchy
+
 extends Node
-func _ready() -> void: if not self.visible: toggle(false)
+func _ready() -> void: 
+	# Make sure process mode for this node is 'always' so when game pauses it doesnt pause this scripts functions aswell
+	process_mode = Node.PROCESS_MODE_ALWAYS 
+	if not self.visible: toggle(false)
 
 func toggle(paused: bool) -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN if paused else Input.MOUSE_MODE_CAPTURED
