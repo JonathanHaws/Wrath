9405aa56d410e9c4ae0af6b1fd76700ce48034c5
JonathanHaws <jonathanmhaws1@gmail.com>
2025-10-28 00:33:13 -0600

    experiementation with weather to make it follow player better. produeral texture and multiple particle texture. Box seems to work best with some space around player. Perhaps multiple systems? Like one with spherical surface that way always some particles on screen atleast? More xperimentation needed

--- actors/props/weather/weather.gd
+++ actors/props/weather/weather.gd
@@ -1,6 +1,22 @@
 extends Node3D
 
+#@export var particles: Node3D # Experimenting with potential fix to have particles follow player even when they move super fast
+#@export var explosiveness_curve: Curve 
+#@export var radius: float = 10
+
+#var last_pos: Vector3
+
+
 func _physics_process(_delta) -> void:
+	#last_pos = global_transform.origin
+	
 	var active_camera = get_viewport().get_camera_3d()
 	if active_camera and is_instance_valid(active_camera):
 		global_transform = active_camera.global_transform
+
+
+	#var distance_moved = global_transform.origin.distance_to(last_pos)
+	
+	#if particles:
+		#particles.explosiveness = explosiveness_curve.sample(distance_moved)
+		#print(particles.explosiveness)
