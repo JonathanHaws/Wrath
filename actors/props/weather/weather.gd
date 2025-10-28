b838baa8d379d643f73e79cfc1dda9962ba8cee2
JonathanHaws <jonathanmhaws1@gmail.com>
2025-10-27 18:32:49 -0600

    got rid of roughness on bricks. The reflections and highlights often grab too much attention for background objects also give a dirty feel. Might work good on characters though. Need to add explosivity to weather to make it when player moves fast to a new area the same amount of particles are occupying the frustum. Tweaking the spawn radius to be larger costs a lot and is wasteful as a lot more particles could be in the players view potentially if implemented right. Allowing for blizzard weather without using so many particles potentially

--- actors/props/weather/weather.gd
+++ actors/props/weather/weather.gd
@@ -3,4 +3,4 @@ extends Node3D
 func _physics_process(_delta) -> void:
 	var active_camera = get_viewport().get_camera_3d()
 	if active_camera and is_instance_valid(active_camera):
-		global_transform.origin = active_camera.global_transform.origin
+		global_transform = active_camera.global_transform
