c21168c1867183384c2585af272783d958795423
JonathanHaws <jonathanmhaws1@gmail.com>
2026-01-03 17:45:48 -0700

    forks on there own line / entry in the json to make files more flexible and readable

--- actors/characters/dialog/dialog.gd
+++ actors/characters/dialog/dialog.gd
@@ -2,22 +2,20 @@ extends Node
 @export var anim: AnimationPlayer ## For entry / exit animations from area
 @export var area: Area3D ## Defines the range in which will activate conversations
 @export var player_group: String = "player" ## Defines the group of bodies which can trigger conversations
-@export var disable_actions := ["attack", "jump"] ## Requires DisableInput global
+
 @export var start_index = 0
 @export var dialog_file: Resource
 @export var SAVE_PROGRESS := false
+@export var disable_actions := ["attack", "jump"] ## Requires DisableInput global to work
 @export var conversation_templates: Array[PackedScene] = [] ## Scenes to spawn / despawn when the conversation is finished (area range exited) [NOT IMPLEMENTED YET]
 
 @export var dialog_templates: Array[PackedScene] = [ ## Scene templates to spawn / despawn when the last sentence is finished
 	load("res://actors/characters/dialog/scenes/chat.tscn"),
-	load("res://actors/characters/dialog/scenes/choice.tscn"),
-	]
-
+	load("res://actors/characters/dialog/scenes/choice.tscn"),]
 @export var dialog_key_map := { ## For shortening dialog files. Specify what key should spawn what scene
 	"say":0, 
-	"choice":1, 
-	}
-
+	"choice":1,}
+	
 var dialog_save_key
 var current_index = start_index
 var in_range = false
@@ -41,22 +39,25 @@ func _end_dialog() -> void:
 	current_index = start_index
 
 func skip_to(value) -> void:
-	if typeof(value) == TYPE_STRING: # Skip to fork
-		for i in range(dialog.size()):
-			var entry = dialog[i]
-			if entry.has("fork") and entry.fork == value:
-				current_index = i
-				return
-	else: # Skip number of sentences
-		current_index += value
-	
+	for i in range(dialog.size()):
+		var entry = dialog[i]
+		if entry.has("fork") and entry.fork == value:
+			current_index = i
+			return
+
 func _spawn_next_dialog() -> void:
 	
 	if not in_range: return
+	
 	if current_index >= dialog.size(): # Loop back
 		current_index = int(start_index)
 		return
+	
+	while current_index < dialog.size() and dialog[current_index].has("fork"): # Skip fork labels
+		current_index += 1	
+		
 	var entry = dialog[current_index]	
+	
 	
 	if "save" in entry and SAVE_PROGRESS:
 		Save.data[dialog_save_key] = current_index
@@ -70,10 +71,8 @@ func _spawn_next_dialog() -> void:
 			instance.tree_exited.connect(_spawn_next_dialog) 
 			add_child(instance)
 			
-	if "skip" in entry: 
-		skip_to(entry.skip)
-	else:
-		current_index += 1
+	if "skip" in entry: skip_to(entry.skip)
+	else: current_index += 1
 		
 	if "anim" in entry and "anim_player_group" in entry :
 		for p in get_tree().get_nodes_in_group(entry.anim_player_group):
