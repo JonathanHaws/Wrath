a715604afaeedad3bf4d4c04e209dca9f0a813b0
JonathanHaws <jonathanmhaws1@gmail.com>
2026-01-06 16:38:23 -0700

    skip fix for gluttony. Advance() is the correct way to fastforward not Seek(). Also added the ability to specify nodes to destroy when skipping

--- menus/skip/skip.gd
+++ menus/skip/skip.gd
@@ -1,15 +1,16 @@
 extends Node
-@export var SKIPPABLE_GROUP: String = "skippable" ## Animation players to skip
-@export var SKIPPABLE_MARKER: String = "skip" ## Where to skip in animations while still updating audio tracks, animations, and calling methods
-@export var ANIMATION_PLAYER: AnimationPlayer
-@export var ANIMATION_SKIPPING_NAME: StringName = &"SKIPPING" 
-@export var ANIMATION_SKIPPED_NAME: StringName = &"SKIPPED" 
+@export var SKIPPABLE_GROUP: String = "skippable" ## Animation players that have skippable animations
+@export var SKIP_MARKER: String = "skip" ## Animation marker name Where to skip in animations while still updating audio tracks, animations, and calling methods
+@export var DESTROY_GROUP: String = "destroy_on_skip" ## Items to destroy when skipping
+@export var ANIMATION_PLAYER: AnimationPlayer ## Animation player for the overlay
+@export var ANIMATION_SKIPPING_NAME: String = "SKIPPING" ## Animation name that fills up skip
+@export var ANIMATION_SKIPPED_NAME: String = "SKIPPED" ## Animation name for when skip has been confirmed
 
 func skippable_animation_playing() -> bool: ## Returns wether theres any animation players playing an animation with skippable marker
 	for node in get_tree().get_nodes_in_group(SKIPPABLE_GROUP):
 		if node is AnimationPlayer and node.is_playing():
-			if SKIPPABLE_MARKER in node.get_animation(node.current_animation).get_marker_names():
-				if node.get_animation(node.current_animation).get_marker_time(SKIPPABLE_MARKER) > node.current_animation_position:
+			if SKIP_MARKER in node.get_animation(node.current_animation).get_marker_names():
+				if node.get_animation(node.current_animation).get_marker_time(SKIP_MARKER) > node.current_animation_position:
 					return true
 	return false
 
@@ -17,12 +18,14 @@ func _skip(play_fade: bool = true) -> void:
 	for node in get_tree().get_nodes_in_group(SKIPPABLE_GROUP):
 		if node is AnimationPlayer and node.is_playing():
 			var anim = node.get_animation(node.current_animation)
-			if SKIPPABLE_MARKER in anim.get_marker_names():
-				var t = anim.get_marker_time(SKIPPABLE_MARKER)
-				node.seek(t, true)
+			if SKIP_MARKER in anim.get_marker_names():
+				var target_time = anim.get_marker_time(SKIP_MARKER)
+				node.advance(target_time - node.current_animation_position)
 	
-	if play_fade and ANIMATION_PLAYER and ANIMATION_SKIPPED_NAME != &"":
-		ANIMATION_PLAYER.play(ANIMATION_SKIPPED_NAME)
+	for node in get_tree().get_nodes_in_group(DESTROY_GROUP):
+		node.queue_free()
+	
+	if play_fade: ANIMATION_PLAYER.play(ANIMATION_SKIPPED_NAME)
 				
 func _process(_delta: float) -> void:
 	if not ANIMATION_PLAYER: return
