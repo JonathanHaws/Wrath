cb1056b42966afe3929643f39ce204dd8ac8d2c0
JonathanHaws <jonathanmhaws1@gmail.com>
2026-01-16 18:37:59 -0700

    gave hurt shape the ability to trigger an animation when something is hit. Easier to tweak freeze frames and camera shake on players hurt box. Rather then having to configure animations for all enemies hit shape. For when the player is being hit and not the not hitting. Hit shape can specify. Saving a lot of work

--- actors/characters/hurt_shape.gd
+++ actors/characters/hurt_shape.gd
@@ -3,6 +3,8 @@ extends Area3D
 @export var damage_spread := 0
 @export var damage_multiplier: float = 1
 @export var damage_groups: Array[String] = ["player_hitshape"] ##area groups you want this hurtbox to damage
+@export var hit_animation_player: AnimationPlayer
+@export var hit_anim: String = "HIT"
 signal hurt_something
 
 func _on_area_entered(area: Area3D) -> void:
@@ -13,6 +15,8 @@ func _on_area_entered(area: Area3D) -> void:
 				#print("hit", damage)
 				area.hit(self, int(damage + randi_range(-damage_spread, damage_spread)) * damage_multiplier)
 			emit_signal("hurt_something")
+			if hit_animation_player:
+				hit_animation_player.play(hit_anim)		
 			break
 
 func _ready() -> void:
