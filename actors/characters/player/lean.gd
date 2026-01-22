4dccc274c9a16c4e16cc6cf91559b861d695bfe8
JonathanHaws <jonathanmhaws1@gmail.com>
2026-01-21 22:07:44 -0700

    migration to 4.5 and leaning improvement. Still needs work though. Ideally if player is running foward and suddenly starts running right. Then they should start leaning foward. Lean back and right. then just right

--- actors/characters/player/lean.gd
+++ actors/characters/player/lean.gd
@@ -2,22 +2,30 @@ extends Node
 @export var BODY: CharacterBody3D
 @export var MESH: Node3D
 @export var LEAN_SPEED := 8.0
-@export var MAX_LEAN_ANGLE := 0.0 # degrees
+@export var MAX_LEAN_ANGLE := 5.0 # degrees
 var last_velocity := Vector3.ZERO
 
 func _physics_process(delta: float) -> void:
 	if not BODY or not MESH: return
 
-	var delta_velocity: Vector3 = BODY.velocity - last_velocity
+	#var delta_velocity: Vector3 = BODY.velocity - last_velocity
 	last_velocity = BODY.velocity
 	
+	#var keyboard_vector := Input.get_vector("keyboard_left", "keyboard_right", "keyboard_forward", "keyboard_back")
+	#var controller_vector := Input.get_vector("controller_left", "controller_right", "controller_forward", "controller_back")
+	#var input_vector := keyboard_vector + controller_vector
+	
 	var forward: Vector3 = -MESH.global_transform.basis.z
-	var lean_amount: float = delta_velocity.dot(forward)
+	
+	#if input_vector.length() > 1.0:
+		#forward = forward
+	#else:
+		#forward = -forward
 
-	var target_lean: float = clamp(-lean_amount * MAX_LEAN_ANGLE, -MAX_LEAN_ANGLE, MAX_LEAN_ANGLE)
+	var target_lean = clamp(BODY.velocity.dot(forward) * MAX_LEAN_ANGLE, -MAX_LEAN_ANGLE, MAX_LEAN_ANGLE)
 
-	var rot: Vector3 = MESH.rotation_degrees
-	rot.x = lerp(rot.x, target_lean, LEAN_SPEED * delta)
+	var rot := MESH.rotation_degrees
+	rot.x = lerp(rot.x, -target_lean, LEAN_SPEED * delta)
 	MESH.rotation_degrees = rot
 
 	
