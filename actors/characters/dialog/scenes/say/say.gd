70a9e32fa1d3bca607aa40df610c95d23ff16b10
JonathanHaws <jonathanmhaws1@gmail.com>
2026-02-16 23:21:18 -0700

    skipping dialog only skips to the end of the line. And then another click skips to the next dialog. From feedback is how most games handle it as it allows speed reading. And if its not the direction can easily remove

--- actors/characters/dialog/scenes/say/say.gd
+++ actors/characters/dialog/scenes/say/say.gd
@@ -1,6 +1,8 @@
 extends AnimationPlayer
 @export var info: String = ""
 @export var label: Label
+@export var reveal_animation_player: AnimationPlayer
+@export var reveal_skipped_animation: String = "SKIPPED"
 
 func _ready():
 	label.text = info
@@ -13,5 +15,9 @@ func spawn_next_dialog() -> void:
 	
 func _process(_delta):
 	if Input.is_action_just_pressed("interact"): 
-		queue("exited")
+		if reveal_animation_player and reveal_animation_player.is_playing() and reveal_animation_player.has_animation(reveal_skipped_animation):
+			reveal_animation_player.play(reveal_skipped_animation, 0)
+		else:
+			queue("exited")	# already done → end
+
 					
