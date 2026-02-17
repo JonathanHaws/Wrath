189d5ee7a58975d2c973437fbd48de02e38bdbfe
JonathanHaws <jonathanmhaws1@gmail.com>
2026-02-17 16:34:37 -0700

    greed fixes, attack hitbox fixes, envy lock on tweak, and simplification and fix of tutorial

--- actors/tutorial_label.gd
+++ actors/tutorial_label.gd
@@ -4,25 +4,21 @@ extends Control
 @export var action_name: String = "interact"
 @export var place_holder_string: String = "[key]"
 @export var capitalize: bool = true
+func set_tutorial_text() -> void:
+	var binding_text: String = Controls.get_action_bindings(action_name)
+	if capitalize: binding_text = binding_text.to_upper()
+	if "text" in label: label.text = label.text.replace(place_holder_string, binding_text)
 
 @export_group("Save")
-@export var save_learnt: bool = true
 @export var saved_key: String = ""
 @export var required_learn_count: int = 3
-func queue_free_if_learnt() -> void:
-	if not save_learnt: return
-	
+func get_key() -> String:
 	var key_name: String = saved_key
 	if key_name.is_empty():
 		key_name = Save.get_unique_key(self, "learn")
-	
-	if int(Save.data.get(saved_key, 0)) >= required_learn_count:
-	
-		if animation_player and learned_animation != "" and animation_player.current_animation != learned_animation:
-			animation_player.connect("animation_finished", Callable(self, "_on_animation_finished"))
-			animation_player.play(learned_animation, 0)
-		else:
-			queue_free()
+	return key_name
+func has_learned_tutorial() -> bool:
+	return int(Save.data.get(get_key(), 0)) >= required_learn_count
 
 @export_group("Animation")
 @export var animation_player: AnimationPlayer ## Animation to play when area is entered
@@ -50,25 +46,28 @@ func _on_body_entered(body: Node) -> void:
 @export var learning_player: AudioStreamPlayer
 
 func _ready():
-	var binding_text: String = Controls.get_action_bindings(action_name)
-	if capitalize: binding_text = binding_text.to_upper()
-	if "text" in label: label.text = label.text.replace(place_holder_string, binding_text)
-	queue_free_if_learnt()
+	set_tutorial_text()
+	if has_learned_tutorial(): queue_free()
 	
 	if trigger_tutorial_with_area:
-		visible = false
-
-		if Save.data.has(saved_key) and int(Save.data[saved_key]) < required_learn_count: ## Stay revealed even if player goes to a different scene
-			visible = true
+		if Save.data.has(saved_key): # if player has key they have already enetered this area in the past
 			if animation_player: animation_player.play(already_revealed_animation, 0)
-		
-		for area in get_tree().get_nodes_in_group(tutorial_area_group_name):
-			area.connect("body_entered", Callable(self, "_on_body_entered"))
+		else:
+			visible = false
+			for area in get_tree().get_nodes_in_group(tutorial_area_group_name):
+				area.connect("body_entered", Callable(self, "_on_body_entered"))
 	else:
 		if animation_player: animation_player.play(reveal_animation, 0)
 	
 func _process(_delta):
-	if save_learnt and Input.is_action_just_pressed(action_name) and visible:
+	if Input.is_action_just_pressed(action_name) and visible:
 		if learning_player: learning_player.play()
-		queue_free_if_learnt()
+
 		Save.data[saved_key] = int(Save.data.get(saved_key, 0)) + 1	# increment
+		
+		if has_learned_tutorial():
+			if animation_player and learned_animation != "" and animation_player.current_animation != learned_animation:
+				animation_player.connect("animation_finished", Callable(self, "_on_animation_finished"))
+				animation_player.play(learned_animation, 0)
+			else:
+				queue_free()
