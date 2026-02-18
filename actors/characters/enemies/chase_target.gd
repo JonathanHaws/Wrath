390302907130ee8fea6ece020347404bfb483a4c
JonathanHaws <jonathanmhaws1@gmail.com>
2026-02-17 17:30:32 -0700

    improvements and clarity to chase_target script

--- actors/characters/enemies/chase_target.gd
+++ actors/characters/enemies/chase_target.gd
@@ -1,26 +1,29 @@
 extends Node3D
-@export_subgroup("Navigate")
-@export var NAV_AGENT: NavigationAgent3D 
-@export var MESH: Node3D ## The mesh that has functions to face the target
-@export var BODY: Node3D ## The body that will move to face
-@export var FLIPPED_TRACKING: bool = false
-@export var MOVE_AND_SLIDE: bool = true ## Stop enemy from glitchy behavior by not chasing anymore once close enough defined by this area
-@export var USE_GRAVITY: bool = true ## Stop enemy from using gravity (flying enemies)
-
-@export_subgroup("Target")
+@export_group("Navigation")
 @export var TARGET_GROUP: String = "player"
+@export var BODY: Node3D ## The body that will move and collide
+@export var MESH: Node3D ## The mesh that faces the target
+@export var NAV_AGENT: NavigationAgent3D 
+@export var INPUT_READ_VELOCITY_MULTIPLIER: float = 0.0 ## Use targets body velocity to predict movement (Fire shot in front of player)
 @export var MATCH_POSITION: bool = true ## Determines whether to match this nodes global position to target
+
+@export_group("Tracking")
 @export var TRACKING_SPEED: float = 5.0 ## Degrees Per Second
 @export var TRACKING_MULTIPLIER: float = 1.0 ## How Quickly rotation will match target
-#
+@export var FLIPPED_TRACKING: bool = false
+
+@export_group("Chase")
+@export var USE_PATHFINDING: bool = true
+@export var MOVE_AND_SLIDE: bool = true ## Stop enemy from glitchy behavior by not chasing anymore once close enough defined by this area
+@export var USE_GRAVITY: bool = true ## Stop enemy from using gravity (flying enemies)
+@export var MESH_CHASE_VECTOR: bool = false ## use mesh forward instead of immeadite next navigation vector
+@export var CHASE_VERTICALLY: bool = false ## If turned off velocity will only ever be applied on the x and z axes
+@export var SPEED = 9.0 ## speed in which target will give chase
+@export var SPEED_MULTIPLIER: float = 1.0  ## Multiplier allowing certain animation to stop chase with easily animatable property
+
 @export_subgroup("Range")
 @export var STOP_CHASE_AREA: Area3D ## What alerts enemies to give chase. If no area is specified they are omincient and always chase
 @export var AWARENESS_AREA: Area3D ## How close they have to be to give chase. If none is specified its everywhere
-
-@export_subgroup("Tracking")
-@export var SPEED = 9.0 ## speed in which target will give chase
-@export var SPEED_MULTIPLIER: float = 1.0  ## Multiplier allowing certain animation to stop chase with easily animatable property
-@export var INPUT_READ_VELOCITY_MULTIPLIER: float = 0.0 ## Use targets body velocity to predict movement (Fire shot in front of player)
 
 var target: Node3D = null
 var should_chase: bool = true
@@ -43,20 +46,25 @@ func track(delta: float) -> void:
 	MESH.rotation = initial_rotation
 	MESH.rotation.y = lerp_angle(initial_rotation.y, target_rotation.y, TRACKING_SPEED * TRACKING_MULTIPLIER * delta)
 
-func move_to_target(speed: float = 0.0, scalar: Vector3 = Vector3(1, 0, 1)) -> void:
+func chase_target(speed: float = 0.0) -> void:
 	if not target or not NAV_AGENT: return
-	NAV_AGENT.target_position = global_transform.origin
+	var chase_velocity: Vector3 
 	
-	var path_vector = NAV_AGENT.get_next_path_position() - BODY.global_transform.origin
-	var navigation_velocity = (path_vector).normalized() * scalar 
+	if USE_PATHFINDING:
+		NAV_AGENT.target_position = global_transform.origin
+		chase_velocity = (NAV_AGENT.get_next_path_position() - BODY.global_transform.origin).normalized()
+	else:	
+		chase_velocity = (global_position - BODY.global_position).normalized() 
 	
-	#var to_player = global_transform.origin - BODY.global_transform.origin
-	var to_player = MESH.global_transform.basis.z
-	if not BODY.is_on_floor():
-		navigation_velocity = to_player.normalized()
+	if not CHASE_VERTICALLY:
+		chase_velocity.y = 0
+		chase_velocity = chase_velocity.normalized()
 	
-	BODY.velocity.x = navigation_velocity.x * speed
-	BODY.velocity.z = navigation_velocity.z * speed
+	if MESH_CHASE_VECTOR:
+		chase_velocity = MESH.global_transform.basis.z.normalized()
+	
+	BODY.velocity.x = chase_velocity.x * speed
+	BODY.velocity.z = chase_velocity.z * speed
 
 func get_closest_from_group_3d(group: String) -> Node3D:
 	var closest = null
@@ -106,7 +114,7 @@ func _physics_process(delta: float) -> void:
 		BODY.velocity.z = 0
 
 		if should_chase:
-			move_to_target(SPEED * SPEED_MULTIPLIER)
+			chase_target(SPEED * SPEED_MULTIPLIER)
 		
 		if MOVE_AND_SLIDE: 
 			BODY.move_and_slide()
