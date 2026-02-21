5cf83bc9afdc8cd1345fa5113b813b027fa9d81a
JonathanHaws <jonathanmhaws1@gmail.com>
2026-02-21 06:20:20 -0700

    massive ghost feeling improvments with various tweaks, changing ghost projectiles. Timings to give fairer punish windows and telegraphs. Also added negative area multipliers with new function in random attack. So that ghosts only shoot when at an outer ring. Likelihoods can be added and subtracted away to make complex areas. Maybe be over thought but allows ghost to only shoot when player is at the very perimiter of range

--- actors/characters/enemies/random_attack.gd
+++ actors/characters/enemies/random_attack.gd
@@ -1,13 +1,30 @@
 extends Node # Child node to animation player to specify attacks based of randomness, promximity, and boss health
+@export var ATTACK_LIKELEHOOD_PER_SECOND: Curve
+@export var TRIGGER_ANIMATIONS: Array[String] = ["CHASE"] ## Requires animation player to be playing specific animations to trigger an attack... If no required trigger will trigger no matter the current animation
+@export var DISABLED: bool = false
 @export var ATTACK_ANIMATION: String = "SLAM"
 @export var LIKELIHOOD_MULTIPLIER: float = 1.0 ## Easy value for adjusting the overall frequency
 @export var ATTACK_RADIUS: float = 10.0
-@export var ATTACK_LIKELEHOOD_PER_SECOND: Curve
-@export var AREA_MULTIPLIER: Area3D ## Eg. Only let backstab happen if behind... Area that specifies whats considered behind... 
-@export var TRIGGER_ANIMATIONS: Array[String] = ["CHASE"] ## Requires animation player to be playing specific animations to trigger an attack... If no required trigger will trigger no matter the current animation
-@export var DISABLED: bool = false
 @export var COOLDOWN: float = 1.0 ## After an attack finishes... How long before they can use it again
 @export var COOLDOWN_REMAINING: float = 1.0 ## Used to disable this attack at the start of the scene
+
+@export_group("Area Multipliers") ## Used to trigger attacks when the player is in specific spots
+@export var AREAS: Array[Area3D] = [] ## The areas which will multiply the liklehood of an attack
+@export var AREA_LIKELIHOOD_VALUES: Array[float] = [] ## Coresponding array to areas which specify how the final likelehood will be affected
+@export var DEFAULT_AREA_LIKELIHOOD: float = 0.0 ## Used if areas exist but none overlap.
+func get_area_likelihood_multiplier() -> float:
+	if AREAS.size() < 1: return 1.0
+	
+	var area_multiplier: float = DEFAULT_AREA_LIKELIHOOD
+	for i in AREAS.size():
+		if i >= AREA_LIKELIHOOD_VALUES.size(): continue
+		var area: Area3D = AREAS[i]
+		var value: float = AREA_LIKELIHOOD_VALUES[i]
+		if area.overlaps_body(TARGET.target):
+			area_multiplier += value
+
+	#print(area_multiplier)
+	return max(area_multiplier, 0.0) 
 
 @export_group("Phase") ## Used for disalbing certain attacks until the proper phase. Accelerating difficulty makes bosses exiciting 
 @export var HITSHAPE: Node ## Percentage determined by health / max_health
@@ -44,9 +61,8 @@ func _physics_process(delta: float) -> void:
 	if distance > ATTACK_RADIUS: proximity_likelihood = 0
 	proximity_likelihood = ATTACK_LIKELEHOOD_PER_SECOND.sample(clamp(distance / ATTACK_RADIUS, 0.0, 1.0))
 	
-	var area_likelihood: float = 1.0
-	if AREA_MULTIPLIER and not AREA_MULTIPLIER.overlaps_body(TARGET.target): area_likelihood = 0.0
-	
+	var area_likelihood: float = get_area_likelihood_multiplier()
+		
 	var chance = proximity_likelihood * area_likelihood * LIKELIHOOD_MULTIPLIER * delta
 	if randf() < chance:
 		ANIM.play(ATTACK_ANIMATION)
