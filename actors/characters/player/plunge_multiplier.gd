5e280ad93fcf364ac2894b929f6d919f6e017e33
JonathanHaws <jonathanmhaws1@gmail.com>
2026-02-21 21:30:47 -0700

    plunge multiplier fixed. Instead of having to worry about finickyk physics process update and is on floor just check if in certain state animation to build up fall

--- actors/characters/player/plunge_multiplier.gd
+++ actors/characters/player/plunge_multiplier.gd
@@ -1,14 +1,15 @@
 extends Node
 @export var PLUNGE_DAMAGE_MULTIPLIER = .3 ## 30 percent more damage each second when falling
-@export var PLAYER: Node ## Node with "falling" property which should return the amount of time theyve been in the air  
+@export var STATE_ANIMATION_PLAYER: AnimationPlayer ## Players state animation player
+@export var PLUNGE_ANIMATIONS: Array[String] = ["PLUNGE", "PLUNGE_FALL","JUMPING", "FALL"]
 @export var ATTACK_AREA: Area3D
+var falling: float = 0.0
 
 func increase_damage_plunge():
 	if ATTACK_AREA: 
-		#print('falling amount: ', falling)
-		#var falling: float = PLAYER.falling
-		#ATTACK_AREA.damage_multiplier = 1.0 + (t * PLUNGE_DAMAGE_MULTIPLIER)
-		ATTACK_AREA.damage_multiplier = 1.0 + (PLAYER.falling * PLUNGE_DAMAGE_MULTIPLIER)
+		#print(falling)
+		ATTACK_AREA.damage_multiplier = 1.0 + (falling * PLUNGE_DAMAGE_MULTIPLIER)
+		falling = 0.0
 
 func reset_damage():
 	if ATTACK_AREA: ATTACK_AREA.damage_multiplier = 1
@@ -19,3 +20,9 @@ func load_plunge_damage()-> void:
 func _ready() -> void:
 	load_plunge_damage()
 	Save.connect("save_data_updated", load_plunge_damage)
+
+func _physics_process(delta: float) -> void:
+	falling += delta
+	
+	if not STATE_ANIMATION_PLAYER.current_animation in PLUNGE_ANIMATIONS:
+		falling = 0.0
