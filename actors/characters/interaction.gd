99eb2308dc04962134c28a66f3bdac0fe1952e3c
JonathanHaws <jonathanmhaws1@gmail.com>
2026-02-25 03:05:32 -0700

    small interaction script improvments and preperation

--- actors/characters/interaction.gd
+++ actors/characters/interaction.gd
@@ -22,9 +22,8 @@ extends Node
 @export_subgroup("Interpolation")## Remember animation tracks properties are update top to bottom in track list
 @export_range(0.0, 10.0, 0.01, "or_greater") var duration_seconds: float = 0.0 ## Determines how long the tween takes to align transforms 
 @export_range(0.0, 1.0, 0.01) var weight: float = 0.5 ## Determines who stays still versus who moves. Usually only want target to move (0). Middle is (0.5)
-@export_range(0.0, 1.0, 0.01) var rotation_weight: float = 0.5
-func _get_middle_transform(target_mesh: Node3D) -> Transform3D:
-	return mesh.global_transform.interpolate_with(target_mesh.global_transform, weight)
+@export var interpolate_position: bool = true
+@export var interpolate_rotation: bool = true
 
 func _on_body_entered(other_body: Node3D) -> void:
 	if disabled: return
@@ -40,7 +39,7 @@ func _match_transforms() -> void:
 	var target_body = get_tree().get_first_node_in_group(target_body_group)
 	if not target_mesh or not target_body: return
 
-	var middle = _get_middle_transform(target_mesh)
+	var middle = mesh.global_transform.interpolate_with(target_mesh.global_transform, weight)
 	
 	if duration_seconds > 0.0:
 		var tween = create_tween()
