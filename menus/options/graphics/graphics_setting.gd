8f156976cb83e56db0b258845056b08ac65da02b
JonathanHaws <jonathanmhaws1@gmail.com>
2026-03-24 01:11:45 -0600

    start of easier graphic settings improvements. For now just seperate glow / glow intensitity settings

--- menus/options/graphics/graphics_setting.gd
+++ menus/options/graphics/graphics_setting.gd
@@ -1,25 +1,33 @@
 extends Control
 @export var setting_name := ""
 const ENVIRONMENT_MAP := {
-	"bloom_enabled": "glow_enabled",
-	"ssao_enabled": "ssao_enabled",
+	"glow": "glow_enabled",
+	"glow_intensity": "glow_intensity",
+	"bloom": "glow_bloom",
+
 	"brightness": "adjustment_brightness",
 	"contrast": "adjustment_contrast",
 	"saturation": "adjustment_saturation",
+	
+	"ssao_enabled": "ssao_enabled",
 	"ssao_radius": "ssao_radius",
 	"ssao_intensity": "ssao_intensity",
 }
+
+func find_self_or_child(type):
+	if is_instance_of(self, type): return self
+	for child in get_children():
+		if is_instance_of(child, type): return child
+	return null
 
 func get_environment() -> Environment:
 	var world := get_viewport().get_world_3d()
 	if not world: return null
 	return world.environment
-
 func get_environment_value(key: String) -> Variant:
 	var env := get_environment()
 	if env == null or not ENVIRONMENT_MAP.has(key): return null
 	return env.get(ENVIRONMENT_MAP[key])
-
 func set_environment_value(key: String, value: Variant) -> void:
 	var env := get_environment()
 	if env == null or not ENVIRONMENT_MAP.has(key): return
@@ -27,12 +35,19 @@ func set_environment_value(key: String, value: Variant) -> void:
 
 func _ready() -> void:
 	
+	if not toggle_button: toggle_button = find_self_or_child(CheckButton)
+	if not slider: slider = find_self_or_child(HSlider)
+	if not display_label: display_label = find_self_or_child(Label)
+	
 	if toggle_button:
 		toggle_button.toggled.connect(_on_toggled)
 		var fallback = get_environment_value(setting_name)
-		toggle_button.button_pressed = fallback
-		_on_toggled(Config.load_setting("display", setting_name, fallback))
+		if fallback:
+			toggle_button.button_pressed = fallback
+			_on_toggled(Config.load_setting("display", setting_name, fallback))
 	
+	if display_label: prefix_text = display_label.text
+#	
 	if slider: 	
 		slider.value_changed.connect(_on_value_changed)
 		var fallback = get_environment_value(setting_name)
@@ -43,15 +58,15 @@ func _ready() -> void:
 @export_group("Toggle Button")
 @export var toggle_button: CheckButton
 func _on_toggled(enabled: bool) -> void:
-	toggle_button.button_pressed = enabled
 	Config.save_setting("display", setting_name, enabled)
 	set_environment_value(setting_name, enabled)
 
 @export_group("Slider")
 @export var slider: HSlider
 @export var display_label: Label
+@onready var prefix_text: String
 func _on_value_changed(new_value: float) -> void:
-	slider.value = new_value
 	Config.save_setting("display", setting_name, new_value)
-	if display_label: display_label.text = str(new_value)
 	set_environment_value(setting_name, new_value)
+	if display_label: display_label.text = prefix_text + str(new_value)
+	
