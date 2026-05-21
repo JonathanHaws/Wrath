5d882cf39bccdfc6088dafdeeb27c36a5f3aaa6f
unknown <jonathanmhaws1@gmail.com>
2026-05-21 09:59:36 -0700

    min step height to solve potential rounding and precision errors with collision system? Regardless below a certain step height. Suspension should be covered by the bottom roundness of the collider shape

--- script/component/step_climber.gd
+++ script/component/step_climber.gd
@@ -3,6 +3,7 @@ extends Node3D ## Script enabling 3D Bodies to not get stuck on small ledges or 
 ## Should usually be the radius of the collision shape of the body plus a little amount 0.01 - 0.1
 @export var STEP_DISTANCE: float = .6 
 @export var MAX_STEP_HEIGHT: float = 1.3 ## How much the player can step up
+@export var MIN_STEP_HEIGHT: float = 0.05 ## Mimium step height
 @export var BODY: CharacterBody3D ## The body that is moved up and retains velocity
 @export var DEBUG_RAY: RayCast3D ## A way to visualize the step probe
 var last_velocity: Vector3 = Vector3.ZERO
@@ -16,12 +17,11 @@ func is_accelerating() -> bool:
 
 func try_step_up() -> void:		
 	
+	#print(BODY.get_slide_collision_count())
 	#if not BODY.is_on_floor(): return 
 	if BODY.velocity.y < 0: return
 	if not BODY.is_on_wall(): return   
 	if not is_accelerating(): return
-	
-	#print(BODY.get_slide_collision_count())
 	
 	for i in BODY.get_slide_collision_count():
 		
@@ -48,24 +48,31 @@ func try_step_up() -> void:
 			DEBUG_RAY.global_position = ray_start
 			DEBUG_RAY.target_position = ray_end - ray_start
 		
-		if not result: return
-		if abs(result.normal.y) <= 0.9: return # dont step if too steep 
+		if not result: 
+			#print('No surface to step on')
+			return
 		
+		if abs(result.normal.y) <= 0.9: 
+			#print('Not stepping. Surface to steep')
+			return 
+	
 		var step = result.position.y - BODY.global_position.y	
-			
-		## Return if would be in collision
 		var new_pos = BODY.global_position
 		new_pos.y += step
 		var params := PhysicsTestMotionParameters3D.new()
 		params.from = global_transform
 		params.motion = new_pos - global_position
 		if PhysicsServer3D.body_test_motion(BODY.get_rid(), params): 
-			#print('colliding')
+			#print('Not stepping. New position would be inside wall... ')
 			return	
+			
+		if step < MIN_STEP_HEIGHT: 	
+			#print('Not stepping. Below minimum step height... Step should be handled by bottom roundness of the collider shape')
+			return
 			
 		BODY.global_position.y += step
 		BODY.velocity = last_velocity
-		#print("Stepping up!" + str(step))	
+		#print("Stepping up! " + str(step))	
 		
 func _physics_process(_delta: float) -> void:
 	
