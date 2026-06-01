7bea26fbaaef85c6af92e86ee0bf0a79c235d558
unknown <jonathanmhaws1@gmail.com>
2026-06-01 10:32:30 -0700

    name collision bug fixed with doors. By validating if the node in the new scene is really a door

--- script/items/door.gd
+++ script/items/door.gd
@@ -1,17 +1,18 @@
-extends Node3D 
+extends Node3D ## Current bug: Must be older then player in scene tree or start wont be ready to position the player
+
 ## Uses a file instead of packed scene to avoid circular dependencies with tscn files.
 ## Still updates references automatically though if reogranizing folders
 @export_file("*.tscn") var DESTINATION_SCENE: String 
 @export var DESTINATION_NODE_NAME: String ## The unique node name of the door to teleport to
-@onready var AREA: Area3D = $Area3D
-@onready var START: Node3D = $Start
+@export var AREA: Area3D = get_node_or_null("Area3D")
+@export var START: Node3D = get_node_or_null("Start")
 
 @export_group("Enter Animation") 
 @export var ANIM: AnimationPlayer
 @export var ANIM_NAME: String = "DOOR"
 
 @export_group("Locking") 
-@export var SAVE_LOCKED: bool = true
+@export var SAVE_LOCKED: bool = false
 @export var LOCKED: bool = false
 
 @export_group("Player") 
@@ -32,21 +33,30 @@ func _load_new_scene() -> void:
 	else:
 		load_player_position()
 
+func find_door_node_or_null(door_name: String) -> Node:
+	for node in get_tree().root.find_children(door_name, "", true, false):
+		if "START" in node: return node # avoid name collision with non door nodes "Exit" for example
+		# add more validating checks if needed
+	return null
+
 func load_player_position() -> void:
 	if not Save.data.has("door_node_name"): return
 	var door_name = Save.data["door_node_name"]
-	var door_node = get_tree().root.find_child(door_name, true, false)
+	var door_node = find_door_node_or_null(door_name)
 	Save.data.erase("door_node_name")
 	#print(door_name)
 	#print(door_node.name)
 
-	if door_node and door_node.START:
+	if door_node and door_node:
 		#print('teleporting player')
 		var player = get_tree().get_first_node_in_group(PLAYER_BODY_GROUP)
+		
+		print(door_node.name)
 		player.global_transform = door_node.START.global_transform
 		player.get_node("Mesh").transform = Transform3D.IDENTITY
 		player.get_node("SpringArm3D").global_basis = door_node.START.global_basis
-		player.CAMERA.global_transform = player.CAMERA.TARGET_NODE.global_transform
+		#player.CAMERA.global_transform = player.CAMERA.TARGET_NODE.global_transform
+		#player.CAMERA.TARGET_NODE.global_transform = player.CAMERA.global_transform
 		player.FADE_IN_ANIM.play("DOOR_FADE_IN")
 
 func _on_area_body_entered(body: Node) -> void:
