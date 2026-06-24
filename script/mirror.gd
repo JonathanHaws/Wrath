0be655845ce2d80312e1f0b000524d6514bf1105
unknown <jonathanmhaws1@gmail.com>
2026-06-23 17:00:42 -0700

    fixed mirror bugs. For some reason ssao causes problems in reflection. So might have to disable that for the subviewport enviroment

--- script/mirror.gd
+++ script/mirror.gd
@@ -6,22 +6,17 @@ extends MeshInstance3D # Use quad mesh. ALso make sure tranform scale = 1 with d
 func _ready():
 	viewport.size = Vector2i(mesh.size * pixels_per_world_unit)
 	mirror_camera.projection = Camera3D.PROJECTION_FRUSTUM
+
 	
-
 #make it so that theres a subviewport with a camera
-#
 #the mirror camera should go reflected transform with frustum... projection with the near being the distance frmo teh mirror camera to the mirror. clipping its side
-
-func reflect_point(point: Vector3, plane_point: Vector3, plane_normal: Vector3) -> Vector3:
-	var to_point = point - plane_point
-	return point - 2.0 * to_point.dot(plane_normal) * plane_normal
-
-func reflect_direction(dir: Vector3, plane_normal: Vector3) -> Vector3:
-	return dir - 2.0 * dir.dot(plane_normal) * plane_normal
 
 func _process(_delta):
 	var current_cam: Camera3D= get_viewport().get_camera_3d()
 	if current_cam == null: return
+
+	if viewport.world_3d == null: viewport.world_3d = World3D.new()
+	viewport.world_3d = get_viewport().world_3d
 
 	viewport.size = Vector2i(mesh.size * pixels_per_world_unit)
 	
@@ -32,9 +27,9 @@ func _process(_delta):
 	
 	#Look perpendicular into the mirror plane for frostum camera
 	mirror_camera.global_transform = mirror_camera.global_transform.looking_at(
-			current_cam.global_transform.origin/2 + mirror_camera.global_transform.origin/2, \
-			mirror_camera.global_transform.basis.y
-		)
+		mirror_camera.global_transform.origin + global_transform.basis.z.normalized(),
+		global_transform.basis.y.normalized()
+	)
 	
 	var cam2mirror_offset = global_transform.origin - mirror_camera.global_transform.origin
 	var near = abs((cam2mirror_offset).dot(MirrorNormal)) # near plane distance
@@ -43,8 +38,7 @@ func _process(_delta):
 	# transform offset to camera's local coordinate system (frostum offset uses local space)
 	var cam2mirror_camlocal = mirror_camera.global_transform.basis.inverse() * cam2mirror_offset
 	var frostum_offset =  Vector2(cam2mirror_camlocal.x, cam2mirror_camlocal.y)
-	var aspect = float(mesh.size.x) / float(mesh.size.y)
-	mirror_camera.set_frustum(mesh.size.y, frostum_offset, near, 10000)
+	mirror_camera.set_frustum(mesh.size.y, frostum_offset, near, 10000) #godot 4.7 seems to use vertical size as frustum... so use that
 
 
 func Mirror_transform(n : Vector3, d : Vector3) -> Transform3D:
